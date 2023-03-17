fb4bc23c62f5004eda8c35ab3de2bcaf40954f8b
Pedro Sanhueza <pedroasd1205@gmail.com>
2023-03-17 15:26:20 -0600

    commit

---
+++ Streamlit-Home/index.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.write('Python and R projects')
+
+st.markdown('''
+<a href="https://pedr-streamlitforbes-com-billionairesforbesbillionaires-arvaca.streamlit.app/">
+  <img src="https://avatars.githubusercontent.com/u/81782049?s=48&v=4" alt="Example Image">
+</a>
+''',unsafe_allow_html=True)
