379ec0bf55ea3dcbce5d377f2241203630d7470a
Pedro Sanhueza <pedroasd1205@gmail.com>
2023-03-17 15:29:31 -0600

    c-ex

--- Streamlit-Home/index.py
+++ Streamlit-Home/index.py
@@ -3,7 +3,8 @@ import streamlit as st
 st.write('Python and R projects')
 
 st.markdown('''
-<a href="https://pedr-streamlitforbes-com-billionairesforbesbillionaires-arvaca.streamlit.app/">
-  <img src="https://avatars.githubusercontent.com/u/81782049?s=48&v=4" alt="Pedro Image">
+<a href="https://www.example.com">
+  <img src="https://www.example.com/images/example-image.jpg" alt="Example Image">
 </a>
+
 ''',unsafe_allow_html=True)
