cc7ed7a224d37c3d6eac62d6460c5e24c742ae52
Pedro Sanhueza <pedroasd1205@gmail.com>
2023-03-28 00:32:03 -0600

    commitcss

--- Analysis-Python/house.gov-representatives/test.py
+++ Analysis-Python/house.gov-representatives/test.py
@@ -5,7 +5,7 @@ st.write("Hello")
 st.markdown('''
 <head>
 	<title>US House of Representatives</title>
-	
+	<link rel="stylesheet" type="text/css" href="style.css">
     <style>
 		img {
 			width: 100%;
