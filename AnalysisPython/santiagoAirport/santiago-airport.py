fe4c25818c05b676ad59cf47e0c21c4e51bce59c
Pedro Sanhueza <pedroasd1205@gmail.com>
2023-05-02 22:53:26 -0600

    ticks

--- AnalysisPython/santiagoAirport/santiago-airport.py
+++ AnalysisPython/santiagoAirport/santiago-airport.py
@@ -283,7 +283,7 @@ with tab2_Descriptive_Statistics:
 		hourly_flights = df_departures.groupby('Hour').count()['Flight'].reset_index(name='Count')
 		chart = alt.Chart(hourly_flights).mark_bar().encode(
 			x=alt.X('Hour:N', title='Hour of the Day',axis=alt.Axis(labelAngle=0)),
-			y=alt.Y('Count:Q', title='Flights',scale=alt.Scale(domain=[0, 100])),color=alt.condition(
+			y=alt.Y('Count:Q', title='',scale=alt.Scale(domain=[0, 100])),color=alt.condition(
 			alt.datum.Count >= hourly_flights.nlargest(2, 'Count')['Count'].min(),
 			alt.value('orange'),alt.value('gray'))
 		).properties(height=700,title=alt.TitleParams(text='Flights per Hour',align='left',subtitle='Departure Flights',subtitleColor='white'),
@@ -296,7 +296,7 @@ with tab2_Descriptive_Statistics:
 		hourly_flights = df_arrivals.groupby('Hour').count()['Flight'].reset_index(name='Count')
 		chart = alt.Chart(hourly_flights).mark_bar().encode(
 			x=alt.X('Hour:N', title='Hour of the Day',axis=alt.Axis(labelAngle=0)),
-			y=alt.Y('Count:Q', title='Flights',scale=alt.Scale(domain=[0, 100])),color=alt.condition(
+			y=alt.Y('Count:Q', title='',ticks=False,scale=alt.Scale(domain=[0, 100])),color=alt.condition(
 			alt.datum.Count >= hourly_flights.nlargest(2, 'Count')['Count'].min(),
 			alt.value('orange'),alt.value('gray'))
 		).properties(height=700,title=alt.TitleParams(text='Flights per Hour', align='left', subtitle='Arrival Flights',subtitleColor='white'),
