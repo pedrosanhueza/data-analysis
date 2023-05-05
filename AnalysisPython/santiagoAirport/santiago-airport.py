94e23310fa2cdbae712c97036a41ac9131c80dd3
Pedro Sanhueza <pedroasd1205@gmail.com>
2023-05-05 09:41:39 -0600

    approximate

--- AnalysisPython/santiagoAirport/santiago-airport.py
+++ AnalysisPython/santiagoAirport/santiago-airport.py
@@ -531,7 +531,7 @@ with tab4_Hypothesis_Testing:
 	contingency_table = pd.crosstab(df_arrivals_terminal['Terminal'], df_arrivals_terminal['Flight'])
 	stat, p, dof, expected = chi2_contingency(contingency_table)
 	
-	st.latex(r'''\text{The chi-square test of independence is performed with a p-value of } %s.''' % p)
+	st.latex(r'''\text{The chi-square test of independence is performed with a p-value of } %s \approx 0''' %p)
 
 with tab5_Regression_Analysis:
 	st.markdown('''
